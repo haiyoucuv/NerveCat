17bf2564d4fef3d0a2885e1a9ee13f8939ac1aa5
haiyoucuv <qiuxu123456@live.com>
2019-06-08 18:03:29 +0800

    Classes/AppDelegate.cpp 	Classes/AppDelegate.h 	Classes/Cat.cpp 	Classes/Cat.h 	Classes/GameScene.cpp 	Classes/GameScene.h 	Classes/Glb.cpp 	Classes/Glb.h 	Classes/Map.cpp 	Classes/Map.h 	Classes/Pot.cpp 	Classes/Pot.h 	Classes/TouchNode.cpp 	Classes/TouchNode.h 	Classes/UINode.cpp 	Resources/

---
+++ Classes/UINode.h
@@ -0,0 +1,26 @@
+﻿#ifndef __UI_NODE_H__
+#define __UI_NODE_H__
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class CUINode : public Node {
+public:
+	CUINode();
+	virtual ~CUINode();
+	virtual bool init();
+	CREATE_FUNC(CUINode);
+
+private:
+	void buttonCallFun(Ref* pSender);
+	CC_SYNTHESIZE_READONLY(Sprite*, m_pVector, ImgVector);
+	CC_SYNTHESIZE_READONLY(Sprite*, m_pFailed, ImgFailed);
+
+private:
+	Sprite* m_pLogo;
+	MenuItemImage* m_pBtnStart;
+	MenuItemImage* m_pBtnReplay;
+};
+
+#endif	//__UI_NODE_H__
